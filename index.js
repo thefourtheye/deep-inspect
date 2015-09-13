54c2aeba18cb2a0c11ece88267f4f90c9aea4cc4
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-09-13 21:27:15 +0530

    improve validations
    This commit allows anything other than primitives to be inspected. Also
    it allows specifying the depth we should crawl back the prototype chain

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ function isNumber(obj) {
   return obj && toString(obj) === '[object Number]';
 }
 
+// Source: isaacs's core-util-is library
+function isPrimitive(arg) {
+  return arg === null ||
+         typeof arg === 'boolean' ||
+         typeof arg === 'number'  ||
+         typeof arg === 'string'  ||
+         typeof arg === 'symbol'  ||
+         typeof arg === 'undefined';
+}
+
 function isInteger(obj) {
   if (Number.isInteger) {
     return Number.isInteger(obj);
@@ -47,35 +57,50 @@ function simpleClone(obj, target) {
   return target;
 }
 
+function deepInspect(obj, options) {
+  var keys,
+      i;
+
+  if (options.showHidden) {
+    keys = Object.getOwnPropertyNames(obj);
+    keys = keys.concat(Object.getOwnPropertySymbols(obj));
+  } else {
+    keys = Object.keys(obj);
+  }
+}
+
 function inspect(obj, options) {
-  if (!isObject(obj)) {
-    throw new TypeError('Only objects can be inspected');
+  if (isPrimitive(obj)) {
+    throw new TypeError('Primitives cannot be inspected');
   }
 
   options = simpleClone(options || {});
 
-  if (options.showHidden === undefined) {
-    options.showHidden = false;
-  }
+  options.showHidden = options.showHidden || false;
 
   if (!isBoolean(options.showHidden)) {
     throw new TypeError('showHidden property must be a boolean');
   }
 
-  if (options.depth === undefined) {
-    options.depth = 2;
+  options.childrenDepth = options.childrenDepth || 2;
+
+  if (!isInteger(options.childrenDepth) || options.childrenDepth <= 0) {
+    throw new TypeError(
+        'childrenDepth property must be a positive, non-zero integer');
   }
 
-  if (!isInteger(options.depth) || options.depth <= 0) {
-    throw new TypeError('depth property must be a positive, non-zero integer');
-  }
-
-  if (options.showInherited === undefined) {
-    options.showInherited = false;
-  }
+  options.showInherited = options.showInherited || false;
 
   if (!isBoolean(options.showInherited)) {
     throw new TypeError('showInherited property must be a boolean');
   }
 
+  options.inheritanceDepth = options.inheritanceDepth || 1;
+
+  if (!isInteger(options.inheritanceDepth) || options.inheritanceDepth <= 0) {
+    throw new TypeError(
+        'inheritanceDepth property must be a positive, non-zero integer');
+  }
+
+  deepInspect(obj, options);
 }
