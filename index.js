6669bcedf44a69129c7b2e33e9f1c666dc9ad72d
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-09-16 22:50:29 +0530

    Refactor to include util module

--- index.js
+++ index.js
@@ -1,61 +1,6 @@
 /*jslint node: true */
 
-'use strict';
-
-function toString(obj) {
-  return Object.prototype.toString.call(obj);
-}
-
-function isObject(obj) {
-  return obj && toString(obj) === '[object Object]';
-}
-
-function isBoolean(obj) {
-  return obj && toString(obj) === '[object Boolean]';
-}
-
-function isNumber(obj) {
-  return obj && toString(obj) === '[object Number]';
-}
-
-// Source: isaacs's core-util-is library
-function isPrimitive(arg) {
-  return arg === null ||
-         typeof arg === 'boolean' ||
-         typeof arg === 'number'  ||
-         typeof arg === 'string'  ||
-         typeof arg === 'symbol'  ||
-         typeof arg === 'undefined';
-}
-
-function isInteger(obj) {
-  if (Number.isInteger) {
-    return Number.isInteger(obj);
-  }
-
-  return isNumber(obj) &&
-         isFinite(obj) &&
-         Math.floor(obj) === obj;
-}
-
-function simpleClone(obj, target) {
-  var keys,
-      i;
-
-  if (!isObject(obj)) {
-    throw new TypeError('Object to be cloned must be an Object');
-  }
-
-  target = target || {};
-  keys = Object.keys(obj);
-  i = keys.length;
-
-  while (i--) {
-    target[keys[i]] = obj[keys[i]];
-  }
-
-  return target;
-}
+var util = require('./util');
 
 function deepInspect(obj, options) {
   var keys,
@@ -70,34 +15,35 @@ function deepInspect(obj, options) {
 }
 
 function inspect(obj, options) {
-  if (isPrimitive(obj)) {
+  if (util.isPrimitive(obj)) {
     throw new TypeError('Primitives cannot be inspected');
   }
 
-  options = simpleClone(options || {});
+  options = util.simpleClone(options || {});
 
   options.showHidden = options.showHidden || false;
 
-  if (!isBoolean(options.showHidden)) {
+  if (!util.isBoolean(options.showHidden)) {
     throw new TypeError('showHidden property must be a boolean');
   }
 
   options.childrenDepth = options.childrenDepth || 2;
 
-  if (!isInteger(options.childrenDepth) || options.childrenDepth <= 0) {
+  if (!util.isInteger(options.childrenDepth) || options.childrenDepth <= 0) {
     throw new TypeError(
         'childrenDepth property must be a positive, non-zero integer');
   }
 
   options.showInherited = options.showInherited || false;
 
-  if (!isBoolean(options.showInherited)) {
+  if (!util.isBoolean(options.showInherited)) {
     throw new TypeError('showInherited property must be a boolean');
   }
 
   options.inheritanceDepth = options.inheritanceDepth || 1;
 
-  if (!isInteger(options.inheritanceDepth) || options.inheritanceDepth <= 0) {
+  if (!util.isInteger(options.inheritanceDepth) ||
+      options.inheritanceDepth <= 0) {
     throw new TypeError(
         'inheritanceDepth property must be a positive, non-zero integer');
   }
---
+++ util.js
@@ -0,0 +1,56 @@
+'use strict';
+
+function toString(obj) {
+  return Object.prototype.toString.call(obj);
+}
+
+function isObject(obj) {
+  return obj && toString(obj) === '[object Object]';
+}
+
+function isBoolean(obj) {
+  return obj && toString(obj) === '[object Boolean]';
+}
+
+function isNumber(obj) {
+  return obj && toString(obj) === '[object Number]';
+}
+
+// Source: isaacs's core-util-is library
+function isPrimitive(arg) {
+  return arg === null ||
+         typeof arg === 'boolean' ||
+         typeof arg === 'number'  ||
+         typeof arg === 'string'  ||
+         typeof arg === 'symbol'  ||
+         typeof arg === 'undefined';
+}
+
+function isInteger(obj) {
+  if (Number.isInteger) {
+    return Number.isInteger(obj);
+  }
+
+  return isNumber(obj) &&
+         isFinite(obj) &&
+         Math.floor(obj) === obj;
+}
+
+function simpleClone(obj, target) {
+  var keys,
+      i;
+
+  if (!isObject(obj)) {
+    throw new TypeError('Object to be cloned must be an Object');
+  }
+
+  target = target || {};
+  keys = Object.keys(obj);
+  i = keys.length;
+
+  while (i--) {
+    target[keys[i]] = obj[keys[i]];
+  }
+
+  return target;
+}
