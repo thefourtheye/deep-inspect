b017004909b3f48a667ffca1ba8dcc1476458541
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-09-17 18:41:17 +0530

    Formatting fixes & export util functions

--- util.js
+++ util.js
@@ -19,11 +19,11 @@ function isNumber(obj) {
 // Source: isaacs's core-util-is library
 function isPrimitive(arg) {
   return arg === null ||
-         typeof arg === 'boolean' ||
-         typeof arg === 'number'  ||
-         typeof arg === 'string'  ||
-         typeof arg === 'symbol'  ||
-         typeof arg === 'undefined';
+    typeof arg === 'boolean' ||
+    typeof arg === 'number' ||
+    typeof arg === 'string' ||
+    typeof arg === 'symbol' ||
+    typeof arg === 'undefined';
 }
 
 function isInteger(obj) {
@@ -32,13 +32,13 @@ function isInteger(obj) {
   }
 
   return isNumber(obj) &&
-         isFinite(obj) &&
-         Math.floor(obj) === obj;
+    isFinite(obj) &&
+    Math.floor(obj) === obj;
 }
 
 function simpleClone(obj, target) {
   var keys,
-      i;
+    i;
 
   if (!isObject(obj)) {
     throw new TypeError('Object to be cloned must be an Object');
@@ -53,4 +53,14 @@ function simpleClone(obj, target) {
   }
 
   return target;
+}
+
+module.exports = {
+  toString: toString,
+  isObject: isObject,
+  isBoolean: isBoolean,
+  isNumber: isNumber,
+  isPrimitive: isPrimitive,
+  isInteger: isInteger,
+  simpleClone: simpleClone
 }
