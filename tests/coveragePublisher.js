22069caf74df023269053cc66e774b389f77dd03
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-10-02 23:20:12 +0530

    Make npm test common for both dev and CI

---
+++ tests/coveragePublisher.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+
+if (process.env.CI) {
+  var args = ['./node_modules/coveralls/bin/coveralls'];
+  var child = spawn(process.execPath, args, {
+    stdio: ['pipe', 1, 2, 'ipc']
+  });
+  fs.createReadStream('./coverage/lcov.info').pipe(child.stdin);
+} else {
+  console.log('Skipping publishing coverage report.');
+}
