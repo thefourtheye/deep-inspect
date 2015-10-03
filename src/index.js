4dae83ccf2d20ef676490600b6b777104b45ebc5
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-10-03 19:41:32 +0530

    Move deepInspect to different file

--- src/index.js
+++ src/index.js
@@ -1,59 +1,6 @@
 var util = require('./util');
+var deepInspect = require('./deep-inspect');
 var archy = require('archy');
-
-function deepInspect(obj, options, cLevel, pLevel) {
-  if (util.isPrimitive(obj) || cLevel === options.depth ||
-    pLevel === options.parentChainLevel) {
-    return util.format(obj);
-  }
-
-  var keys;
-
-  if (options.showHidden) {
-    keys = Object.getOwnPropertyNames(obj);
-    if (util.hasSymbolsSupport) {
-      keys = keys.concat(Object.getOwnPropertySymbols(obj));
-    }
-  } else {
-    keys = Object.keys(obj);
-  }
-
-  if (keys.length === 0 && !options.parentChainLevel) {
-    if (util.isObject(obj)) {
-      return '{}\n';
-    }
-    if (Array.isArray(obj)) {
-      return '[]\n';
-    }
-    if (util.isFunction(obj)) {
-      return '[Function "' + obj.name + '"]\n';
-    }
-    throw new TypeError('Unexpected type: ' + util.toString(obj));
-  }
-
-  var result = {};
-  result.label = util.getTypeName(obj);
-  result.nodes = [];
-
-  if (options.parentChainLevel) {
-    result.nodes.push(deepInspect(Object.getPrototypeOf(obj),
-      options, 0, pLevel + 1));
-  }
-
-  var indexRE = /^["]?\d+["]?$/;
-
-  result.nodes = result.nodes.concat(keys.map(function (key) {
-    var fKey = util.format(key);
-    var title = Array.isArray(obj) && indexRE.test(fKey) ? 'Index: ' :
-      'Key: ';
-    return {
-      label: title + fKey,
-      nodes: [deepInspect(obj[key], options, cLevel + 1, pLevel)]
-    };
-  }));
-
-  return result;
-}
 
 function inspect(obj, options) {
   options = util.simpleClone(options || {});
